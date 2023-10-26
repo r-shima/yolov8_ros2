31319a0756ab27847e58e287c6ca4bdb40217398
Rintaroh Shima <rintarohshima2023@u.northwestern.edu>
2023-10-26 15:53:43 -0500

    Created object_detection_interfaces. Can publish detection results.

--- object_detection/scripts/yolov8_detection.py
+++ object_detection/scripts/yolov8_detection.py
@@ -6,6 +6,7 @@ from ament_index_python.packages import get_package_prefix
 import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor
+from object_detection_interfaces.msg import ObjectDetection, DetectionResult
 from ultralytics import YOLO
 
 class YOLOv8Detection(Node):
@@ -30,12 +31,40 @@ class YOLOv8Detection(Node):
         self.results = self.model.predict(source=self.rtsp_url, show=True, verbose=False,
                                           stream=True)
 
+        self.detection_pub = self.create_publisher(DetectionResult, 'detection_results', 10)
         self.frequency = 20
         self.timer = self.create_timer(1 / self.frequency, self.timer_callback)
 
+    def publish_detections(self, results):
+        detection_msg = DetectionResult()
+
+        for result in results:
+            num_detections = len(result.boxes.xyxy)
+
+            for i in range(num_detections):
+                object_detection = ObjectDetection()
+
+                # Get the xyxy coordinates
+                box_xyxy = result.boxes.xyxy[i].cpu().numpy()
+                object_detection.x1 = box_xyxy[0].astype(float)
+                object_detection.y1 = box_xyxy[1].astype(float)
+                object_detection.x2 = box_xyxy[2].astype(float)
+                object_detection.y2 = box_xyxy[3].astype(float)
+
+                # Get the confidence value
+                object_detection.confidence = result.boxes.conf[i].cpu().item()
+
+                # Get the class value
+                object_detection.class_value = int(result.boxes.cls[i].cpu().item())
+
+                detection_msg.detections.append(object_detection)
+
+        self.detection_pub.publish(detection_msg)
+    
     def timer_callback(self):
         for result in self.results:
             result.plot()
+            self.publish_detections(result)
 
 def main(args=None):
     rclpy.init(args=args)
